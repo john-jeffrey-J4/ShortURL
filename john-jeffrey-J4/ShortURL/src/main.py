ad8ae4ef0f83bf95be74b8400079a6463c6cb9a8
john-jeffrey-J4 <123716538+john-jeffrey-J4@users.noreply.github.com>
2024-06-12 12:36:54 +0530

    Update main.py

--- john-jeffrey-J4/ShortURL/src/main.py
+++ john-jeffrey-J4/ShortURL/src/main.py
@@ -1,6 +1,6 @@
 import hashlib
-import json
 from appwrite.client import Client
+from appwrite.services.databases import Databases
 import os
 
 
@@ -15,7 +15,12 @@ def main(context):
     #     .set_project(os.environ["APPWRITE_FUNCTION_PROJECT_ID"])
     #     .set_key(os.environ["APPWRITE_API_KEY"])
     # )
-
+    client = Client()
+    (client
+     .set_endpoint('https://cloud.appwrite.io/v1')  # Your API Endpoint
+     .set_project('5df5acd0d48c2')  # Your project ID
+     .set_key('919c2d18fb5d4...a2ae413da83346ad2')  # Your secret API key
+     )
     # You can log messages to the console
     context.log("Hello, Logs!")
 
@@ -26,6 +31,14 @@ def main(context):
     if context.req.method == "GET":
         # Send a response with the res object helpers
         # `ctx.res.send()` dispatches a string back to the client
+        databases = Databases(client)
+        all_data = databases.list_documents(
+            database_id=66694407002556133624,
+            collection_id="666944250024f4a2b507"
+        )
+        from datum in all_data['documents']:
+            context.log(f"{datum}")
+
         return context.res.send("Hello, World!")
     if context.req.method == "POST":
         req_data = context.req.body
@@ -38,9 +51,7 @@ def main(context):
             )
         shortened_url = hashlib.sha256(url_to_shorten.encode()).hexdigest()[:8]
         full_shortened_url = f"{shortened_url}"
-        
-        
-        
+
         return context.res.json(
             {
                 "original_url": url_to_shorten,
