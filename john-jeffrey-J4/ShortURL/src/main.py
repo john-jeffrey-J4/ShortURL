2a5038c49fd08e6c1a9979063a9988539328e091
john-jeffrey-J4 <123716538+john-jeffrey-J4@users.noreply.github.com>
2024-06-12 16:04:53 +0530

    Update main.py

--- john-jeffrey-J4/ShortURL/src/main.py
+++ john-jeffrey-J4/ShortURL/src/main.py
@@ -8,68 +8,74 @@ import uuid
 
 
 def main(context):
+    # Why not try the Appwrite SDK?
+    #
+    # client = (
+    #     Client()
+    #     .set_endpoint("https://cloud.appwrite.io/v1")
+    #     .set_project(os.environ["APPWRITE_FUNCTION_PROJECT_ID"])
+    #     .set_key(os.environ["APPWRITE_API_KEY"])
+    # )
     client = Client()
     (client
      .set_endpoint('https://cloud.appwrite.io/v1')  # Your API Endpoint
      .set_project('666929470005a01fd522')  # Your project ID
      .set_key('42b747d591fb0d006152c329021807af8eb2c05f77d44808fbc6c91e28ddc73d4987fc7c85d1e584c8da035a8d1babd931643095288c7f1fcef2c29105c6379b24f915ace30be0e79ab551144a8266406034cba6935cb6c9cc7b4dde519f69b4e2c775b4b9879cc89dafe352155929a6605ab726a15f21fe572444ec1fe0ab0d')  # Your secret API key
      )
+    # You can log messages to the console
     context.log("Hello, Logs!")
+
+    # If something goes wrong, log an error
     context.error("Hello, Errors!")
     databases = Databases(client)
-
-    # Set CORS headers
-    def set_cors_headers(res):
-        res.set_header("Access-Control-Allow-Origin", "*")
-        res.set_header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-        res.set_header("Access-Control-Allow-Headers", "Content-Type")
-        return res
-
     if context.req.method == "OPTIONS":
-        res = context.res
-        res = set_cors_headers(res)
-        return res.send('')
-
+        return context.res.set_headers({
+            'Access-Control-Allow-Origin': 'https://shortener-front-end-ten.vercel.app',
+            'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        }).send('')
+        
+    # The `ctx.req` object contains the request data
     if context.req.method == "GET":
+        # Send a response with the res object helpers
+        # `ctx.res.send()` dispatches a string back to the client
+
         path_param = context.req.path
         context.log(path_param)
         all_data = databases.list_documents(
             database_id="66694407002556133624",
             collection_id="666944250024f4a2b507"
         )
-        res = context.res
-        res = set_cors_headers(res)
-
         if path_param == "/listall":
             all_data_return = []
             for datum in all_data['documents']:
                 all_data_return.append({"original_url": datum.get(
                     'originalurl'), "shortened_url": datum.get('hashurl')})
 
-            return res.json({
+            return context.res.set_headers({
+                'Access-Control-Allow-Origin': 'https://shortener-front-end-ten.vercel.app',
+            }).json({
                 "data": all_data_return
             })
         else:
+
             for datum in all_data['documents']:
                 if datum.get('hashurl') == path_param[1:]:
                     redirect_url = datum.get('originalurl')
                     context.log(redirect_url)
-                    return res.redirect(f'{redirect_url}', 301)
+                    return context.res.redirect(f'{redirect_url}', 301)
 
-            return res.send("Data not Found")
+            return context.res.send("Data not Found")
 
     if context.req.method == "POST":
         req_data = context.req.body
-        url_to_shorten = req_data.get('url')
-        if not url_to_shorten:
-            res = context.res
-            res = set_cors_headers(res)
-            return res.json(
+        url_to_shorten = req_data['url']
+        if "url" not in req_data:
+            return context.res.json(
                 {
                     "error": "URL parameter is missing",
                 },
             )
-
         shortened_url = hashlib.sha256(url_to_shorten.encode()).hexdigest()[:8]
         full_shortened_url = f"{shortened_url}"
 
@@ -79,9 +85,7 @@ def main(context):
         )
         for datum in all_data['documents']:
             if datum.get('hashurl') == full_shortened_url:
-                res = context.res
-                res = set_cors_headers(res)
-                return res.json(
+                return context.res.json(
                     {
                         "original_url": url_to_shorten,
                         "shortened_url": full_shortened_url,
@@ -97,18 +101,15 @@ def main(context):
         )
         context.log(result)
 
-        res = context.res
-        res = set_cors_headers(res)
-        return res.json(
+        return context.res.json(
             {
                 "original_url": url_to_shorten,
                 "shortened_url": full_shortened_url,
             }
         )
 
-    res = context.res
-    res = set_cors_headers(res)
-    return res.json(
+    # `ctx.res.json()` is a handy helper for sending JSON
+    return context.res.json(
         {
             "motto": "Build like a team of hundreds_",
             "learn": "https://appwrite.io/docs",
